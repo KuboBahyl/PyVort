812a4ae7b6513239ec9dbe212168bd5fd5cbccc7
KuboBahyl <jakub.bahyl@vacuumlabs.com>
2017-12-05 20:00:17 +0100

    refactoring, better architecture

--- Ring.py
+++ Ring.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3
 #coding: utf-8
-
+#%%
 from vortices import Vortex
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 
 import numpy as np
 
-
-T = 1.5 #Kelvins
-N = 100 #segments
-
-
 ## VORTEX RING, T=0
+
+N=100
 
 radius = 1                #ring radius [A]
 X0 = Y0 = Z0 = 0          #ring center coords [A]
 R0 = np.array([X0,Y0,Z0])
 phi = 2*np.pi/N
 
-ringPositions = np.array([ R0 + 
-                          [0, radius*np.sin(i*phi), radius*np.cos(i*phi)] 
-                          for i in range (N) 
+ringPositions = np.array([ R0 + [0, radius*np.sin(i*phi), radius*np.cos(i*phi)] 
+                           for i in range (N)
                           ])
+#%%
 
+
+## INITIALISATION
 Rvortex = Vortex(ringPositions)
 
-# need to 
+# fixing neigbours for ring
+Rvortex.segments[0]['backward'] = N - 1
+Rvortex.segments[N-1]['forward'] = 0
 
+# adding properties
+Rvortex.addDerivatives()
+
+
+
+#%%
 mpl.rcParams['legend.fontsize'] = 10
 
 fig = plt.figure()
--- vortices.py
+++ vortices.py
@@ -14,51 +14,62 @@ class Vortex(object):
         self.N = len(positions)
         self.segments = np.array([
                 {'coords' : positions[i],
-                 'forward' : i+1,
-                 'backward' : i-1} 
+                 'backward' : i-1,
+                 'forward' : i+1} 
                 for i in range(self.N) 
                 ])
-    
-        # add derivatives
-        for i in range(2,self.N-2):
-            self.segments[i]['tangent'] = self.add_derivative(i)
-            self.segments[i]['curvature'] = self.add_derivative(i, order=2)
-        
+   
     def __repr__(self):
         return 'Quantum Vortex object. Check documentation for available methods.'
     
-    def add_derivative(self, index, order=1, area=5):
-        neighbourCoords = []
-        loc_index = int((area-1)/2)
-        for i in range(area):
-            neigh_index = index - loc_index + i
-            neighbourCoords.append(self.segments[neigh_index]['coords'])
-        
-        h = np.linalg.norm(neighbourCoords[loc_index-1] - neighbourCoords[loc_index])
+    def go_backward(self, item):
+        return self.segments[item['backward']]
+    
+    def go_forward(self, item):
+        return self.segments[item['forward']]     
+    
+    def calc_derivative(self, item, order=1, dist=2):
+        neighCoords = []
+        firstItem = self.go_backward(self.go_backward(item))
+        thisItem = firstItem
+        for i in range(2*dist+1):
+            neighCoords.append( thisItem['coords'] )
+            thisItem = self.go_forward(thisItem)
+            
+        loc_index = dist
+        h = np.linalg.norm(neighCoords[loc_index-1] - neighCoords[loc_index])
         
         if (order == 2):
-            ders = (- 1*neighbourCoords[loc_index-2] 
-                    + 16*neighbourCoords[loc_index-1] 
-                    - 30*neighbourCoords[loc_index+0]
-                    + 16*neighbourCoords[loc_index+1]
-                    - 1*neighbourCoords[loc_index+2] ) / (12*h)
+            ders = (- 1*neighCoords[loc_index-2] 
+                    + 16*neighCoords[loc_index-1] 
+                    - 30*neighCoords[loc_index+0]
+                    + 16*neighCoords[loc_index+1]
+                    - 1*neighCoords[loc_index+2] ) / (12*h)
             return ders
         
         else:        
-            ders = (  1*neighbourCoords[loc_index-2] 
-                    - 8*neighbourCoords[loc_index-1] 
-                    + 0*neighbourCoords[loc_index+0]
-                    + 8*neighbourCoords[loc_index+1]
-                    - 1*neighbourCoords[loc_index+2] ) / (12*h)
+            ders = (  1*neighCoords[loc_index-2] 
+                    - 8*neighCoords[loc_index-1] 
+                    + 0*neighCoords[loc_index+0]
+                    + 8*neighCoords[loc_index+1]
+                    - 1*neighCoords[loc_index+2] ) / (12*h)
             return ders
     
     def get_index(self, position):
         if (position > 0):
-            return position-1
+            if self.segments[position]['forward'] == position + 1:
+                return position
+            else:
+                raise Exception('TODO searching in segments')
         else: raise TypeError('Position should be positive integer')
         
-        
-        
+######################################
+### USER'S OPTIONS
+######################################
+    def addDerivatives(self):
+        for item in self.segments:
+            item['tangent'] = self.calc_derivative(item)
+            item['curvature'] = self.calc_derivative(item, order=2)
         
     def getCoords(self, position):
         index = self.get_index(position)
